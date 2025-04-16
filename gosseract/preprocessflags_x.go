73c4e82dd5e56de88d5247a49f6acb5b42e908c9
binbinzhishang <57622380+binbinzhishang@users.noreply.github.com>
2025-04-16 09:44:11 +0800

    Delete gosseract directory

--- gosseract/preprocessflags_x.go
+++
@@ -1,9 +0,0 @@
-//go:build !darwin
-
-package gosseract
-
-// #cgo CXXFLAGS: -std=c++0x
-// #cgo CPPFLAGS: -I/usr/local/include
-// #cgo CPPFLAGS: -Wno-unused-result
-// #cgo LDFLAGS: -L/usr/local/lib -lleptonica -ltesseract
-import "C"
